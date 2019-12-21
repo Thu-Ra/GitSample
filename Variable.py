67be1066b395d195e889e4d31412aba361be116a
Thu-Ra <thuraminoo116@gmail.com>
2019-12-21 20:11:00 +0630

    Variable

---
+++ Variable.py
@@ -0,0 +1,114 @@
+#21.12.2019
+
+>>> 2+2
+4
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> round(5.0)
+5
+>>> 5.123456
+5.123456
+>>> round(5.123456,2)
+5.12
+>>> 17 / 3
+5.666666666666667
+>>> 17 // 3
+5
+>>> 18 // 4
+4
+>>> 20 // 5
+4
+>>> 17 % 3
+2
+>>> 17 * 4 % 3
+2
+>>> 3 ** 2
+9
+>>> 3 ** 9
+19683
+>>> 7 ** 3
+343
+a = 1
+a (variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+vol = width * height
+vol
+
+sale = 30000
+tax = 5 / 100
+total_tax = sale * tax
+total_price = sale + total_tax
+total
+
+>>> print('spam eggs')
+>>> print(""yes"")
+>>> '"Yes," they said.'
+'"Yes," they said.'
+>>> "\"Yes,\" they said."
+'"Yes," they said.'
+>>> '"Isn\'t" they said.'
+'"Isn\'t" they said.'
+
+>>> s = 'First Line.\nSecond Line.'
+>>> print(s)
+First Line.
+Second Line.
+>>> print("""\
+...
+... Usage: thingy
+...     -a
+...     -b
+...     -c
+... """)
+
+Usage: thingy
+        -a
+        -b
+        -c
+
+>>> 3 * 'un' + 'ium'
+'unununium'
+>>> 3 * "A"
+'AAA'
+>>> 2 * 'BC'+ 'DE'
+'BCBCDE'
+>>> 10 * 'GH' + 3
+
+>>> 'Py''thon'
+'Python'
+>>> text = ('Put several strings within  parnetheses ''to have them joined together.')
+>>> text
+'Put several strings within  parnetheses to have them joined together.'
+>>> word = 'programming'
+>>> word[0]
+'p'
+>>> word[5]
+'a'
+>>> word[-1]
+'g'
+>>> word[8]
+'i'
+>>> word[-2]
+'n'
+>>> word[-9]
+'o'
+>>> word[3:5]
+'gr'
+>>>
+>>> word[4:9]
+'rammi'
+>>> word[:5]
+'progr'
+>>> word[8:]
+'ing'
+>>> word[-2:5]
+''
+>>> word[5:-3]
+'amm'
+>>> word[:2] + word[3:]
+'prgramming'
+>>>
